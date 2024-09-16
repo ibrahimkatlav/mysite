650c29ca763b28efb29292b5af047c9eba5d78b3
ibrahimkatlav <ibrahimkatlav@gmail.com>
2024-09-17 00:05:13 +0300

    d
    d

--- .netlify/build/chunks/ai-remove-image-background_DmQUYyVx.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./ai-remove-image-background_DJE8nYZY.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [".prose[data-astro-cid-yu3cdcui] img[data-astro-cid-yu3cdcui]{border-radius:20px}\n"];
-					const collectedScripts = [{"props":{"src":"/_astro/main.astro_astro_type_script_index_1_lang.As3cppGa.js","type":"module"},"children":""},{"props":{"src":"/_astro/main.astro_astro_type_script_index_0_lang.DF2keTmI.js","type":"module"},"children":""}];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
